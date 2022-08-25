695befd10ee36a42d49faf4655378757f82bba07
sergiogonzalez24 <sergiomanuel2020@gmail.com>
2022-08-24 20:48:25 -0500

    .

---
+++ OpenMP/build/CMakeFiles/OpenMP.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenMP.dir/main.cpp.o"
+  "CMakeFiles/OpenMP.dir/main.cpp.o.d"
+  "OpenMP"
+  "OpenMP.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OpenMP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ OpenMP/main.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+#pragma omp parallel for
+    for(int i=0;i<10;i++){
+        printf("%i\n",i);
+    }
+    return 0;
+}
